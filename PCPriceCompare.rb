8ed86dd0e1f022da59f210dc17cd47961e275310
Matt Osbourne <mattosbourne@News-MBP.home>
2016-05-13 16:19:01 -0400

    program now displays the average price of the top three listed items from Google Shopping

--- PCPriceCompare.rb
+++ PCPriceCompare.rb
@@ -13,15 +13,30 @@ google_form = page.form('gs')
 item = gets.strip
 google_form.q = item
 page = agent.submit(google_form, google_form.buttons.first)
-pp page.uri.to_s
+#pp page.uri.to_s
 url = page.uri.to_s
 
 
 page = Nokogiri::HTML(open(url))
-puts page.class
-puts page.css('title')
-puts page.css('b')[0].text
-amount = page.css('b')[0].text
-amount.delete! '$'
-amount = amount.to_f
-pp amount
+#puts page.class
+#puts page.css('title')
+#puts page.css('div div b')[0].text
+#puts page.xpath("//div//div//b").text
+
+total = 0
+count = 0
+loop_count = 0
+while loop_count < 3 do
+  if page.css('div div b')[count].text.include? "$"
+    loop_count += 1
+    amount = page.css('div div b')[count].text
+    amount.delete! '$'
+    amount = amount.to_f
+    total += amount
+    #pp total
+  end
+  count += 1
+end
+total /= 3
+total = total.round(2)
+pp "The average price of a #{item} is $#{total}"
